a7e6f5c777fcdd3dfa9c12eb8e31c12f5e5a1d10
Seenupsr2020 <seenupsr2020@gmail.com>
2026-06-22 21:25:17 +0530

    feat: remove craftsmanship timeline section

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ import AtmosphereBackground from '../components/luxury/AtmosphereBackground';
 import NewArrivals from '../components/NewArrivals';
 import Testimonials from '../components/Testimonials';
 import PackagingShowcase from '../components/luxury/PackagingShowcase';
-import CraftsmanshipTimeline from '../components/luxury/CraftsmanshipTimeline';
 import StoreFront from '../components/luxury/StoreFront';
 
 import ConciergeAssistant from '../components/luxury/ConciergeAssistant';
@@ -41,10 +40,6 @@ export default function Home() {
         <PackagingShowcase />
       </div>
 
-      {/* New Luxury Section 5: Craftsmanship Timeline */}
-      <div className="relative z-10">
-        <CraftsmanshipTimeline />
-      </div>
 
 
 
