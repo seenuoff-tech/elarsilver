500028f52de59573ebbda7c3178c0b326c773318
Seenupsr2020 <seenupsr2020@gmail.com>
2026-06-22 21:34:03 +0530

    chore: disable next.js dev indicators

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  devIndicators: {
+    appIsrStatus: false,
+    buildActivity: false,
+  },
 };
 
 export default nextConfig;
