6262cf1d9010f870a22628b0a322ad3ac51d812a
Seenupsr2020 <seenupsr2020@gmail.com>
2026-07-10 19:21:18 +0530

    Remove static export config and generateStaticParams to allow dynamic routing for products

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -4,13 +4,6 @@ import { newArrivalsData } from '../../../data/newArrivals';
 import { shopProducts } from '../../../data/shopProducts';
 import { mensProducts } from '../../../data/mensProducts';
 
-export function generateStaticParams() {
-  const newArrivalsIds = newArrivalsData.map(p => ({ id: p.id.toString() }));
-  const mensIds = mensProducts.map(p => ({ id: p.id.toString() }));
-  const shopIds = shopProducts.map(p => ({ id: p.id.toString() }));
-  
-  return [...newArrivalsIds, ...mensIds, ...shopIds];
-}
 
 export default function Page() {
   return <ProductClient />;
